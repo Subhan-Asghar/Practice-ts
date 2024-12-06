9bf3e5ced1b9be2eff09dbac75ecdfa10a3a2350
Subhan Asghar <subhanasghar16@gmail.com>
2024-12-07 00:38:24 +0500

    array and tuple

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-var num = 123;
-var str = "hello world";
-var bool = false;
-var toit = 1234;
-console.log(num, str, bool, toit);
+var num = [1, 2, 3, 4, 5, 6, 7];
+var str = ["food", "vegetable"];
+console.log(num[0], str[1]);
+var mix = [1, 2, 3, "ad"];
+console.log(mix[2], mix[3]);
+//Tuples
+var num_tup = [123, "table"];
+console.log(num_tup[1], num_tup[0]);
