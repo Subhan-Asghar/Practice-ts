3dbfa1e9ba5284e620900b92d15b30773fcfb620
Subhan Asghar <subhanasghar16@gmail.com>
2024-12-07 14:26:44 +0500

    functions

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-var num = [1, 2, 3, 4, 5, 6, 7];
-var str = ["food", "vegetable"];
-console.log(num[0], str[1]);
-var mix = [1, 2, 3, "ad"];
-console.log(mix[2], mix[3]);
-//Tuples
-var num_tup = [123, "table"];
-console.log(num_tup[1], num_tup[0]);
+function printhello() {
+    console.log("hello world");
+}
+function addnum(a, b) {
+    return a + b;
+}
+function mul(a, b) {
+    if (b === void 0) { b = 1; }
+    return a * b;
+}
+// Arrow function 
+var arrow = function (str) {
+    return str;
+};
+printhello();
+console.log(addnum(5, 3));
+console.log(mul(5, 2));
+console.log(arrow("think again "));
