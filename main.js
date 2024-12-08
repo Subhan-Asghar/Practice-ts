a40f74f1951d31b4bafff721924975c0ed457247
Subhan Asghar <subhanasghar16@gmail.com>
2024-12-08 13:57:00 +0500

    union and intersection

--- main.js
+++ main.js
@@ -1,33 +1,9 @@
-//object 
-var user = {
+var c = 234;
+var b = "abc";
+console.log(c, "", b);
+var num = {
     name: "subhan",
-    date: "12/12/1211"
+    age: 1234,
 };
-console.log(user);
-console.log(user.name);
-console.log(user.date);
-user.name = "Ali",
-    console.log(user.name);
-var person = {
-    name: "Subhan",
-    age: 23,
-    price: 34,
-    tax: 10,
-    calculate: function (price, tax) {
-        return price + tax;
-    }
-};
-console.log(person);
-console.log(person.calculate(23, 10));
-var std = {
-    name: "Subhan",
-    age: 123,
-    marks: 45,
-};
-var marks = function (std) {
-    if (std.marks > 33) {
-        return true;
-    }
-    return false;
-};
-console.log(marks(std));
+console.log(num.age);
+console.log(num);
