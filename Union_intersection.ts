c9cb6833f3d7ee03978db49f0fbffa4dfe0b95f5
Subhan Asghar <subhanasghar16@gmail.com>
2024-12-08 14:42:18 +0500

    pratice union and intersection

--- Union_intersection.ts
+++ Union_intersection.ts
@@ -16,3 +16,66 @@ let num:z={
 }
 console.log(num.age)
 console.log(num)
+
+//Practice Question 
+
+// Basic Union Type
+type shape="Circle"|"Square";
+const getArea=(Shape:shape,size:number):number=>{
+    if(Shape=="Circle"){
+        return 3.14*size*size;
+    }
+    else if(Shape=="Square"){
+        return size*size;
+    }
+    return 0;
+
+}
+console.log(getArea("Circle",20));
+console.log(getArea("Square",10));
+
+// Discriminated Union
+type success={type:"success",
+            message:string
+}
+type error={
+    type:"error",
+    errorcode:number,
+}
+const printMessage=(type:success|error)=>{
+    if(type.type=="success"){
+        console.log(type.type)
+        console.log(type.message)
+    }
+    else if(type.type=="error"){
+        console.log(type.type)
+        console.log(type.errorcode)
+    }
+   
+
+}
+const s:success={
+    type:"success",
+    message:"hello world"
+}
+const e:error={
+    type:"error",
+    errorcode:123
+}
+printMessage(s)
+console.log("-----------------")
+printMessage(e)
+
+// Intersection
+
+let obj1={
+    name:"subhan"
+}
+let obj2={
+    age:1223
+}
+const merge=<T,U>(obj1:T,obj2:U):T & U=>{
+    return {...obj1,...obj2};
+}
+const merge_A=merge(obj1,obj2)
+console.log(merge_A)
--- main.js
+++ main.js
@@ -1,9 +1,56 @@
-var c = 234;
-var b = "abc";
-console.log(c, "", b);
-var num = {
-    name: "subhan",
-    age: 1234,
+//Practice Question 
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
 };
-console.log(num.age);
-console.log(num);
+var getArea = function (Shape, size) {
+    if (Shape == "Circle") {
+        return 3.14 * size * size;
+    }
+    else if (Shape == "Square") {
+        return size * size;
+    }
+    return 0;
+};
+console.log(getArea("Circle", 20));
+console.log(getArea("Square", 10));
+var printMessage = function (type) {
+    if (type.type == "success") {
+        console.log(type.type);
+        console.log(type.message);
+    }
+    else if (type.type == "error") {
+        console.log(type.type);
+        console.log(type.errorcode);
+    }
+};
+var s = {
+    type: "success",
+    message: "hello world"
+};
+var e = {
+    type: "error",
+    errorcode: 123
+};
+printMessage(s);
+console.log("-----------------");
+printMessage(e);
+// Intersection
+var obj1 = {
+    name: "subhan"
+};
+var obj2 = {
+    age: 1223
+};
+var merge = function (obj1, obj2) {
+    return __assign(__assign({}, obj1), obj2);
+};
+var merge_A = merge(obj1, obj2);
+console.log(merge_A);
