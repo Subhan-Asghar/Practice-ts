b2675a7eceaed883c93ff0109cfd4cb10abc1b2b
Subhan Asghar <subhanasghar16@gmail.com>
2024-12-09 17:56:07 +0500

    Generics

--- main.js
+++ main.js
@@ -1,29 +1,11 @@
-var Filepermission;
-(function (Filepermission) {
-    Filepermission[Filepermission["Read"] = 1] = "Read";
-    Filepermission[Filepermission["Write"] = 2] = "Write";
-    Filepermission[Filepermission["ReadWrite"] = 3] = "ReadWrite";
-})(Filepermission || (Filepermission = {}));
-console.log(Filepermission.Read);
-console.log(Filepermission.Write);
-console.log(Filepermission.ReadWrite);
-// 2
-var Season;
-(function (Season) {
-    Season[Season["Summer"] = 0] = "Summer";
-    Season[Season["Winter"] = 1] = "Winter";
-    Season[Season["Spring"] = 2] = "Spring";
-    Season[Season["Autumn"] = 3] = "Autumn";
-})(Season || (Season = {}));
-var getSeasonMessage = function (season) {
-    switch (season) {
-        case Season.Summer:
-            return "it's Summer holidays";
-        default:
-            return "dsfdsfdsf";
-    }
-    // if(season==0){
-    //     return "it's Summer holidays"
-    // }
+var arr = [1, 2, 3, 4, 5, 6];
+var printnum = function (arr) {
+    return arr[0];
 };
-console.log(getSeasonMessage(Season.Summer));
+console.log(printnum(arr));
+var arr_s = ["sdas", "sadas", "tyr"];
+var printanyfirst = function (arr) {
+    return arr[0];
+};
+console.log(printanyfirst(arr));
+console.log(printanyfirst(arr_s));
