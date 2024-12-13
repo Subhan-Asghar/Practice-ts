722b58cbba937acb09f4cff24e21271082b051c6
Subhan Asghar <subhanasghar16@gmail.com>
2024-12-13 23:52:26 +0500

    (import/export)

--- main.ts
+++ main.ts
@@ -1,20 +1,3 @@
-let s: string = "abc";
-
-const check = (s: string): Promise<string> => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(s);
-        }, 50000);
-    });
-};
-
-async function fch() {
-    try {
-        const s1: string = await check(s); 
-        console.log(s1);
-    } catch (err) {
-        console.log("Error", err); 
-    }
-}
-
-fch();
+import {add,sub} from './utils'
+console.log(add(2,3))
+console.log(sub(2,3))
---
+++ utils.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = exports.sub = void 0;
+var sub = function (a, b) {
+    return a - b;
+};
+exports.sub = sub;
+var add = function (a, b) {
+    return a + b;
+};
+exports.add = add;
